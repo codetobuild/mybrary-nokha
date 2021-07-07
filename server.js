f05548063127dfeb292df9a3f598d87306590ccc
codetobuild <goodnakha@gmail.com>
2021-07-07 23:48:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true, limit:"1mb"}))
+
+const mongoose = require('mongoose')
+mongoose.connect( 'mongodb://localhost:27017/test' , {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+ console.log('database-connected')
+});
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
+app.listen(process.env.PORT || 3000)
+  
